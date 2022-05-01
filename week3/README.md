0d70f34aa7e1ca43e90908ec7db5740e6d7d61c1
Thomas Yang <thomasyang18@gmail.com>
2022-05-01 10:35:48 -0400

    Finished DCE

--- week3/README.md
+++ week3/README.md
@@ -8,20 +8,13 @@ Ideally, they should produce the same outputs; cherry on top is less instruction
 
 Go backwards. 
 
-First, if this function has any "side effects", don't remove it (If you call a function from it or print, for example)
+First, if this function has any "side effects", don't remove it (If you call a function from it or print)
 
 Next, if this function had no side effects, we can consider removing it if:
 
 The destination is marked as unusued.
 
 If the instruction wasn't removed, mark the destination as used and continue.
-
-
-
-# LVN Algorithm
-
-
-
 
 # DCE Tests
 
@@ -40,3 +33,16 @@ A chain of useless instructions that keep reassigning the same variable, followe
 ## chainingmult
 
 A chain of only useful instructions (well, useful disregarding const prop)
+
+## jumping
+
+Jump and break statements mixed in to make sure those are getting optimized properly. For example, only remove one instruction if you compute a boolean twice and then jump somewhere.
+
+#DCE Results
+
+ALl passed!
+
+# LVN Algorithm
+
+# LVN Tests
+
--- week3/optimize.py
+++ week3/optimize.py
@@ -1,8 +1,11 @@
 import json
 import sys
+import os
 
-#Code from last week
 TERMINATORS = 'jmp', 'br', 'ret'
+
+DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG'] == 'True'
+
 def form_blocks(body):
 	cur_block = []
 	for instr in body:
@@ -23,73 +26,105 @@ def form_blocks(body):
 		yield cur_block
 
 
-def print_blocks(blocks):
-	for block in blocks:
-		print("-----------------------")
-		for instr in block:
-			print(instr)
-		print("-----------------------")
-
-# Takes a basic block and does DCE on it
-def DCE(body):
-	while True:
-		kept = []
-		is_used = {}
-		for instr in reversed(body):
-			keep = True
-
-			if 'dest' in instr:
-				if instr['dest'] not in is_used:
-					keep = False	
-			
-			if 'args' in instr:
-				for arg in instr['args']:
-					is_used[arg] = True
-			
-			if keep:
-				kept.append(instr)
-			
-		kept.reverse()		
-
-		if len(kept) == len(body):
-			body = kept
-			break
-
-		body = kept
-
+#Takes a basic block  and does DCE Locally on it
+def DCE_Local(body):	
+	used_vars = {}
+	res = []
 	
+	for instr in body:
+		if 'dest' in instr:
+			used_vars[instr['dest']] = True
+	
+	for instr in reversed(body):
+		skip = False
+		if 'dest' in instr:
+			if instr['dest'] not in used_vars:
+				skip = True
+			else:
+				used_vars.pop(instr['dest'])
+		
+		if 'args' in instr:
+			for arg in instr['args']:
+				used_vars[arg] = True
+
+		if not skip:
+			res.append(instr)
+		
+	res.reverse()
+	return res
+
+SIDE_EFFECTS = 'call', 'print'
+JMPS = 'jmp', 'br'
+
+# Takes a function and does DCE globally on it
+def DCE_Global(body):
+	used = {}
+	for instr in body:
+		if 'args' not in instr:
+			continue
+		for arg in instr['args']:
+			used[arg]=True
+	
+	res = []
+	for instr in body:
+		res.append(instr)
+		if 'dest' not in instr:
+			#probably an important instruction like label or jmp or something, continue
+			continue
+		if instr['dest'] in used:
+			#used so continue
+			continue
+		
+		if 'op' in instr and (instr['op'] in SIDE_EFFECTS or instr['op'] in JMPS):
+			#if it's a call or print function, never remove it, if it's a jmp or br, never remove it
+			continue
+		res.pop()
+	
+	return res
+
+def DCE(body):
+	# Iterate to convergence; by that probably just count # of instructions or someth
+
+	for i in range(0, len(body)+5):
+		temp_body = []
+		
+		for block in form_blocks(body):
+			for instr in DCE_Local(block):
+				temp_body.append(instr)	
+		
+		body = DCE_Global(temp_body)
+
 	return body
 
-# Takes a basic block and does LVN on it 
+# Takes a function and does LVN on it 
 def LVN(body):
-	return None
-
-# Takes a list of blocks, and performs optimization opt_f() on each block
-def optimize(blocks, opt_f):
-	ret_blocks = []
-	for block in blocks:
-		ret_blocks.append(opt_f(block))
-
-	return ret_blocks
+	return body
 
 # Takes a program from standard input and optimizes it with DCE and LVN
 def optimize_blocks():
 	prog = json.load(sys.stdin)
 	
-	blocks = []	
+	functions = []	
 
 	for func in prog['functions']:
-		for block in form_blocks(func['instrs']):
-			blocks.append(block)
-	
-	print_blocks(blocks)	
-	
-	#blocks = optimize(blocks, LVN)
-	blocks = optimize(blocks, DCE)
 
-	print("After Optimization")
+		if DEBUG:
+			print("Before: ")
+			print(func['instrs'])
+	
+		func['instrs'] = LVN(func['instrs'])
+		func['instrs'] = DCE(func['instrs'])
+	
+		if DEBUG:
+			print("After :")
+			print(func['instrs'])
 
-	print_blocks(blocks)
+		functions.append(func)
+
+	prog['functions'] = functions
+	
+	if not DEBUG:
+		json.dump(prog, sys.stdout, indent=2)
 
 if __name__ == "__main__":
 	optimize_blocks()
